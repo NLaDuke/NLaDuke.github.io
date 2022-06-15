91ec6c79d86411851f470b65cfcadd9bb2b54425
Nolan LaDuke <NALaDuke09@gmail.com>
2022-06-15 15:00:22 -0400

    Published Base Files (still WIP)

---
+++ webpage.js
@@ -0,0 +1,12 @@
+/*
+// Element Getters
+const links = document.querySelectorAll('.link-icon');
+
+// Hover Event Listeners:
+links.forEach(link=>{
+  link.addEventListener("mouseover", function(event){
+    link.classList.remove('material-icons-outlined');
+    link.classList.add('material-icons-two-tone')
+  })
+})
+*/
