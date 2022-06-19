95a183b8ac49937144ac8d80abd84c48327663d5
Nolan LaDuke <NALaDuke09@gmail.com>
2022-06-18 20:16:07 -0400

    added work-experience section

--- webpage.js
+++ webpage.js
@@ -1,12 +1,61 @@
-/*
-// Element Getters
-const links = document.querySelectorAll('.link-icon');
 
-// Hover Event Listeners:
-links.forEach(link=>{
-  link.addEventListener("mouseover", function(event){
-    link.classList.remove('material-icons-outlined');
-    link.classList.add('material-icons-two-tone')
-  })
-})
-*/
+// Element Getters
+const timeline_col = document.getElementsByClassName("timeline-element-collapses");
+
+//Smooth Timeline Transition Animation Functions:
+function collapseSection(element){
+  var sectionHeight = element.scrollHeight;
+  var elementTransition = element.style.transition;
+  element.style.transition = '';
+  //Restore clickability after transition ends
+  element.addEventListener('transitionend', function(elem){
+    element.removeEventListener('transitionend', arguments.callee);
+    element.parentElement.style.pointerEvents = 'auto';
+    element.parentElement.classList.toggle("timeline-element-transitioning");
+  });
+
+  requestAnimationFrame(function(){
+    element.style.height = sectionHeight + 'px';
+    element.style.transition = elementTransition;
+
+    requestAnimationFrame(function(){
+      element.style.height = 0 + 'px';
+    });
+  });
+
+  element.setAttribute('data-collapsed', 'true');
+
+}
+
+function expandSection(element){
+  var sectionHeight = element.scrollHeight;
+  element.style.height = sectionHeight+'px';
+  //Restore clickability after transition ends
+  element.addEventListener('transitionend', function(elem){
+    element.removeEventListener('transitionend', arguments.callee);
+    element.style.height = null;
+    element.parentElement.style.pointerEvents = 'auto';
+    element.parentElement.classList.toggle("timeline-element-transitioning");
+  });
+  element.setAttribute('data-collapsed', 'false');
+}
+
+// Collapsable Timeline elements
+Array.from(timeline_col).forEach(elem => {
+  //Collapse on page load
+  collapseSection(elem.querySelector(".timeline-collapsable"));
+  // Add Click Event
+  elem.addEventListener("click", function(){
+    elem.style.pointerEvents = 'none'; //Prevent clicks to avoid animation errors
+    elem.classList.toggle("timeline-element-active");
+    elem.classList.toggle("timeline-element-transitioning");
+    var content = elem.querySelector(".timeline-collapsable");
+    var isCollapsed = content.getAttribute('data-collapsed') === 'true';
+    if (!isCollapsed){
+      collapseSection(content);
+    }
+    else{
+      expandSection(content);
+    }
+  });
+});
